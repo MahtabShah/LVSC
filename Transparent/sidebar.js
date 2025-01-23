8a0b8aebb33e697a1c60c9add39f32ca1eb24d85
Mahtab Shah <mahtab.shah916@gmail.com>
2025-01-23 18:34:02 +0530

    Update sidebar.js

--- Transparent/sidebar.js
+++ Transparent/sidebar.js
@@ -1,8 +1,4 @@
 const leftSide = document.querySelector(".left");
-leftSide.style.display = 'none';
 document.querySelector(".Mahtabfiles").addEventListener('click', () => {
     leftSide.classList.toggle('d-initial');
-    if(!leftSide.classList.contains('d-initial')){
-        leftSide.style.display = 'none';
-    }
 });
