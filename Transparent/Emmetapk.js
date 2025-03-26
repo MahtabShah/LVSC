a2b91ecee219d3e5e6844a59deb7b0835d9f17e9
Mahtab Shah <mahtab.shah916@gmail.com>
2025-03-26 19:53:52 +0530

    Update Emmetapk.js

--- Transparent/Emmetapk.js
+++ Transparent/Emmetapk.js
@@ -20,11 +20,45 @@ function emmetGenerator(str){
   let tagName = str.match(/[\w]+/g);
   let attr = "class";
   let content = "content"
-  
-  function TagtGenerator(tagName, attr , content ){
-    let element = document.createElement(tagName);
-    element.append(content);
-  }
-  
+  let AllTags = [];
+  let Prelement =  document.createElement("div");    
+  return TagtGenerator(tagName, Prelement , 0 , sign)
+
   
 }
+
+
+function TagtGenerator(tagName, j , Prelement , i , sign){
+
+   if(j > tagName.length ){
+      return "";
+   }
+
+   let element = document.createElement(tagName[j]); 
+   
+    if (sign[i] === ">") {
+         element.append(TagtGenerator(tagName , j+1 , element , i+1 , sign));
+    }else if(sign[i] === "+"){
+        Prelement.append(TagtGenerator(tagName, j+1, element , i+1 , sign));
+    }else if(sign[i] === "*"){
+      for (let k = 0; k < tagName[j+1]; k++) {
+        Prelement.append(TagtGenerator(tagName, j+2 , element , i+1 , sign));
+      }
+      
+    }
+   return element;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
