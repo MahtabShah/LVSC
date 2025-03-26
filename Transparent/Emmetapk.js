e9b53d651679e334d61964e1856fd82d0d44c199
Mahtab Shah <mahtab.shah916@gmail.com>
2025-03-26 17:59:24 +0530

    Update Emmetapk.js

--- Transparent/Emmetapk.js
+++ Transparent/Emmetapk.js
@@ -7,7 +7,16 @@ let str = '';
 codeditor.addEventListener("input" , (e)=>{
   str +=e.key;
 
+  // class adiding
   if(str.charAt(0) === "." && e.key === "Enter"){
     let out = `<div class=${str.slice(1)}></div>`;
    }
-})
+});
+
+
+function emmetGenerator(str){
+
+  let sign = str.replace(/[\w]+/g , "").split("");
+  let syntax = str.match(/[\w]+/g);
+  
+}
