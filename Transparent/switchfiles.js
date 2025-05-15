1857ffa0ec117e374135d46b0788ee6fddbca7f7
Mahtab Shah <mahtab.shah916@gmail.com>
2025-05-15 08:46:46 +0530

    Update switchfiles.js

--- Transparent/switchfiles.js
+++ Transparent/switchfiles.js
@@ -151,6 +151,9 @@ function updateEditor() {
     <head>
        <meta charset="UTF-8">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+       <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
        <title>Document</title>
        <style>${styleCSStextArea.value}</style>
        <style>${style}</style>
