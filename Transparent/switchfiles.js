db49d1548fced155d6c3661db121b269e8fa257d
Mahtab Shah <mahtab.shah916@gmail.com>
2025-05-19 12:07:49 +0530

    Update switchfiles.js

--- Transparent/switchfiles.js
+++ Transparent/switchfiles.js
@@ -176,7 +176,7 @@ function updateEditor() {
     <body>
       ${textContent[1]}
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3xJpX3VjKb2iQkpZMb+Ci3TcWn9X+gDjZ+3qgQnUTo1JvHcEXG62Lf+dqKjp9zF" crossorigin="anonymous"></script>
-      <script>
+      <script type="text/babel">
        ${scriptJStextArea.value}
       <\/script>
     <\/body>
