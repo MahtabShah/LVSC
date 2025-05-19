188ff398d0040450eedfa8e4f6bec22bbaf0defd
Mahtab Shah <mahtab.shah916@gmail.com>
2025-05-19 12:29:47 +0530

    Update switchfiles.js

--- Transparent/switchfiles.js
+++ Transparent/switchfiles.js
@@ -134,9 +134,11 @@ FilesClicker.forEach(f => {
     
 // }
 
+let tailwindCondition = false;
 
 function updateEditor() {
     // let reactTXTareaValue = getReactTXTValue() || " ";
+    
 
     let textContent = textarea.value;
     const text = textContent.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace('/', '&#47;');
@@ -163,6 +165,10 @@ function updateEditor() {
        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
        <!-- Bootstrap 5.3 CSS -->
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+       
+       <!-- tailwindcss -->
+      ${tailwindCondition ?  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"> : ""}
+
        <!-- React & Babel CDN -->
        <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
        <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
