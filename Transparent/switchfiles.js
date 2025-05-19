594b981109cd2b1b7198886b1f9677df866c855e
Mahtab Shah <mahtab.shah916@gmail.com>
2025-05-19 12:00:39 +0530

    Update switchfiles.js

--- Transparent/switchfiles.js
+++ Transparent/switchfiles.js
@@ -128,15 +128,15 @@ FilesClicker.forEach(f => {
 
 // let reactTXTareaValue = "";
 
-function getReactTXTValue() {
-        const iframe = document.getElementById("reactTXTarea");
-        return iframe.contentWindow.document.getElementById("reactTXTarea").value || " ";
+// function getReactTXTValue() {
+//         const iframe = document.getElementById("reactTXTarea");
+//         return iframe.contentWindow.document.getElementById("reactTXTarea").value || " ";
     
-}
+// }
 
 
 function updateEditor() {
-    let reactTXTareaValue = getReactTXTValue() || " ";
+    // let reactTXTareaValue = getReactTXTValue() || " ";
 
     let textContent = textarea.value;
     const text = textContent.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace('/', '&#47;');
@@ -175,11 +175,7 @@ function updateEditor() {
     </head>
     <body>
       ${textContent[1]}
-      ${reactTXTareaValue}
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3xJpX3VjKb2iQkpZMb+Ci3TcWn9X+gDjZ+3qgQnUTo1JvHcEXG62Lf+dqKjp9zF" crossorigin="anonymous"></script>
-       <script type="text/babel" >
-       ${scriptJStextArea.value}
-      <\/script>
       <script>
        ${scriptJStextArea.value}
       <\/script>
