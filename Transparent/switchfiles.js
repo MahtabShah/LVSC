5598827326e462371e5539d1afad2663372fa71a
Mahtab Shah <mahtab.shah916@gmail.com>
2025-05-20 12:26:37 +0530

    Update switchfiles.js

--- Transparent/switchfiles.js
+++ Transparent/switchfiles.js
@@ -175,9 +175,8 @@ function updateEditor() {
        <!-- Bootstrap 5.3 CSS -->
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
        
-       <!-- tailwindcss -->
-      // ${tailwindCondition ?  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"> : ""}
-
+       <!-- tailwindcss tailwindCondition <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"> : ""} -->
+       
        <!-- React & Babel CDN -->
        <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
        <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
